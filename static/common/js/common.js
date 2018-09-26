1e5f99d7334be2f6c04d3270bc0c01381159bd4e
冯鑫龙 <18862005869@163.com>
2018-09-26 18:06:48 +0800

    修改页面

--- static/common/js/common.js
+++ static/common/js/common.js
@@ -1,19 +1,14 @@
 //加在首页和尾页
 $("#top").load("head.html");
 $("#buttom").load("foot.html");
-//部分的js事件
-//鼠标在头像显示或者隐藏相关信息
-$("#top").on('mouseenter','.header-right',function() {
-    //获取自定义属性，来判断显示什么--如果已经登陆，则显示个人信息，否则显示登陆注册窗口
-    var isclick = $(".imghead").attr("isClick");
-    if(isclick == false) {
-        console.log(1111);
-    } else {
-        $(".head-mess").show();
-    }
-    
-});
-$("#top").on('mouseleave','.header-right',function() {
-    $(".head-mess").hide();
-});
-//获取自定义属性
+
+
+
+///////------------方法---------------//1
+
+//获取拿到的token数据然后写入localStorage
+function getRealToken() {
+    localStorage.setItem('real-home-token', 'fdsfs');
+    localStorage.removeItem('real-home-token');
+    return localStorage.getItem('real-home-token');
+}
