d2569bda760212a2f6a56708bf10a7e655b6f2f3
冯鑫龙 <18862005869@163.com>
2018-12-07 17:49:19 +0800

    11

--- static/home/js/index.js
+++ static/home/js/index.js
@@ -499,3 +499,13 @@ function rightTopShow(data) {
     }
     return str;
 }
+
+
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
